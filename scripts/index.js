c9bab269d5bfc27194f2e1a0ec5586eecb28ef42
micah_kapussien <micah@africomltd.com>
2021-03-31 21:00:51 +0300

    Js logic working. Can now generate :)

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,88 @@
 const generateButton = document.getElementById("generate-button");
 const inputClass = document.getElementById("class-input");
+const outputText = document.getElementById("output-text");
+const copyButton = document.getElementById("copy");
 
 var classText;
 generateButton.addEventListener("click", (e) => {
 	inputClass.value = `class Session {
-        private $id;
-        private $safSessionId;
-        private $msisdn;
-        private $serviceCode;
-        private $ussdString;
-        private $userType;
-        private $status;
-        private $additionalData;
-        private $createdOn;
-        private $createdBy;
-        private $lastUpdatedOn;
-        private $lastUpdatedBy;
-    }`;
+	    private $id;
+	    private $safSessionId;
+	    private $msisdn;
+	    private $serviceCode;
+	    private $ussdString_6;
+	    private $userType_7_;
+	    private $status;
+	    private $additionalData;
+	    private $createdOn;
+	    private $createdBy;
+	    private $lastUpdatedOn;
+	    private $lastUpdatedBy;
+	}`;
 	classText = inputClass.value;
 	getterSetter = generate(classText);
-    alert(getterSetter);
-    
+	outputText.value = getterSetter;
 });
 
 function generate(classText) {
-	//return classText.match(/class [A-Za-z]+ \{/);
-    var properties = classText.match(
+	var result = "";
+	var properties = classText.match(
 		/(private|public|protected) *\$[A-Za-z_]+[1-9]*[A-Za-z_]* *;/gi
 	);
-    for (n in properties) {
-        console.log(properties[n])
-    }
-	return "Nothing";
+	for (n in properties) {
+		property = properties[n];
+		var nameWithDollarSign = property.match(
+			/\$[A-Za-z_]+[1-9]*[A-Za-z_]* */gi
+		)[0];
+		var nameWithoutDollarSign = nameWithDollarSign.match(
+			/[A-Za-z_]+[1-9]*[A-Za-z_]* */gi
+		)[0];
+
+		camelCaseName = nameWithDollarSign.replace(
+			/\$[a-zA-Z]|_[a-zA-Z0-9]|_+/gi,
+			(x) => {
+				letters = x.match(/[A-Za-z0-9]/gi);
+
+				if (letters == null || letters.length <= 0) {
+					return "";
+				}
+				return letters[0].toUpperCase();
+			}
+		);
+
+		var getter =
+			` public function get` +
+			camelCaseName +
+			`()
+    {
+        return $this->` +
+			nameWithoutDollarSign +
+			`;
+    }`;
+
+		var setter =
+			`public function set` +
+			camelCaseName +
+			`(` +
+			nameWithDollarSign +
+			`)` +
+			`
+    {
+        $this->` +
+			nameWithoutDollarSign +
+			` =` +
+			nameWithDollarSign +
+			`;
+    }`;
+
+		console.log(camelCaseName);
+
+		result += setter + "\n" + getter + "\n";
+	}
+	return result;
 }
+
+copyButton.addEventListener("click", (e) => {
+    document.querySelector("#output-text").select();
+	document.execCommand("copy");
+});
